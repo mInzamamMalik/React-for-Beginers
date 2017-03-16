bf8f89807e58e677707b3de159e9db5a014b9a5c
Inzamam Malik <malikasinger1@gmail.com>
2017-03-16 20:20:16 +0500

    rename

--- step08_React-Redux/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers } from 'redux';
-import CounterReducer from './reducer/reducer-counter';
-
-const allReducers = combineReducers({
-    CounterReducer //provide all reducers here comma seaprated
-})
-
-const store = createStore(allReducers)
-export default store;
