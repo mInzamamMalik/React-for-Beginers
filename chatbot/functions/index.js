77b45841d9768e0fa3f28a188c16b716b9a05523
Inzamam Malik <malikasinger1@gmail.com>
2017-07-27 18:47:03 +0500

    parameter revolved done

--- chatbot/functions/index.js
+++ chatbot/functions/index.js
@@ -1,6 +1,8 @@
 var functions = require('firebase-functions');
-var requestify = require('requestify');
 var http = require('./helper');
+var apiai = require('apiai');
+var app = apiai("f87d6ff5226e45b8aec8fb939e0ff38e");
+
 
 exports.chatbot = functions.database.ref('/chatbot/messages/{pushId}')
     .onWrite(event => {
@@ -12,22 +14,29 @@ exports.chatbot = functions.database.ref('/chatbot/messages/{pushId}')
         const query = data.text;
         console.log("you said: ", query);
 
-        return requestify
-            .request('https://api.api.ai/api/query?v=20150910&query=' + query + '&lang=en&sessionId=898aae69-9d7d-4dd3-abeb-721ca2a44bb6&timezone=2017-03-24T21:10:33+0500', {
-                method: 'GET',
-                headers: {
-                    'Authorization': 'Bearer 7f3251bd4aca4decb21d536f886e9f0b'
-                }
-            })
-            .then(function (response) {
-                const reply = response.getBody().result.fulfillment.speech;
-                console.log("reply is: ", reply);
-                return event.data.ref.parent.push({
-                    text: reply,
-                    from: "bot",
-                    to: "zia"
-                });
+        var request = app.textRequest(query, {
+            sessionId: 'unique-session-id'
+        });
+
+        request.on('response', function (response) {
+            console.log(response);
+
+            const reply = response.result.fulfillment.speech;
+            console.log("reply is: ", reply);
+            return event.data.ref.parent.push({
+                text: reply,
+                from: "bot",
+                to: "zia"
             });
+        });
+
+        request.on('error', function (error) {
+            console.log("eror: ", error);
+            return
+        });
+
+        request.end();
+        return
     });
 
 
@@ -40,30 +49,81 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 })
 
 
+
+//Sample response
+// var response = {
+//     speech: "this is speech from firebase",
+//     displayText: "this is displayText from firebase",
+//     data: { "this": "is data from firebase" },
+//     contextOut: [
+//         {
+//             "name": "weather",
+//             "lifespan": 2,
+//             "parameters": { "city": "Rome" }
+//         }
+//     ],
+//     source: "this is source from firebase",
+//     followupEvent: {
+//         name: "signup",
+//         data: {
+//             'givenname': "john",
+//             'lastname': "john"
+//         }
+//     },
+// }
 exports.webhook = http.post((req, res) => {
     console.log("this is body: ", req.body);
     console.log("this is headers: ", req.headers);
+    var body = req.body;
 
-    var response = {
-        speech: "this is speech from firebase",
-        displayText: "this is displayText from firebase",
-        data: { "this": "is data from firebase" },
-        contextOut: [
-            {
-                "name": "weather",
-                "lifespan": 2,
-                "parameters": { "city": "Rome" }
-            }
-        ],
-        source: "this is source from firebase",
-        followupEvent: {
-            name: "signup",
-            data: { 
-                'givenname': "john",
-                'lastname': "john"
-             }
-        },
+    console.log("action: ", body.result.action);
+    switch (body.result.action) {
+
+        case "do-signup":
+
+            //do signup process here
+            //then
+            res.send({
+                speech: "you are signed up",
+                contextOut: [
+                    {
+                        "name": "init",
+                        "lifespan": 0
+                    },
+                    {
+                        "name": "quiz",
+                        "lifespan": 5,
+                        "parameters": { "index": "1" }
+                    }
+                ],
+            })
+            break;
+
+        case "next-question":
+
+            console.log("in next question params: ", body.result.contexts);
+
+            var currentIndex = parseInt(body.result.contexts[0].parameters.index);
+            res.send({
+                speech: "quest index " + (currentIndex + 1),
+                contextOut: [
+                    {
+                        "name": "quiz",
+                        "lifespan": 5,
+                        "parameters": { "index": currentIndex + 1 }
+                    }
+                ],
+            })
+            break;
+
+        default:
+            res.send({
+                speech: "action not found"
+            });
+            break;
     }
+
+
     console.log("response: ", response);
     res.json(response);
 });
